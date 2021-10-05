bf8c88d69d5a53ad9bd71123cac61b173208320b
nguyenxuandieu2204 <nguyenxuandieu2204@gmail.com>
2021-10-05 10:10:58 +0700

    update share button & KKSK

--- js/typed-custom.js
+++ js/typed-custom.js
@@ -1,16 +1,25 @@
-jQuery(document).ready(function() {
-        $(function () {
-// jquery typed plugin
+jQuery(document).ready(function () {
+  $(function () {
+    // jquery typed plugin
     $(".typed").typed({
-        stringsElement: $('.typed-strings'),
-        typeSpeed: 100,
-        backDelay: 500,
-        loop: true,
-        contentType: 'html', // or text
-        // defaults to false for infinite loop
-        loopCount: false,
-        callback: function () { null; },
-        resetCallback: function () { newTyped(); }
+      stringsElement: $(".typed-strings"),
+      typeSpeed: 100,
+      backDelay: 500,
+      loop: true,
+      contentType: "html", // or text
+      // defaults to false for infinite loop
+      loopCount: false,
+      callback: function () {
+        null;
+      },
+      resetCallback: function () {
+        newTyped();
+      },
     });
-});
+  });
+
+  // share button
+  $("#s-icons").click(function () {
+    $(".navbar-nav").toggleClass("show");
+  });
 });
